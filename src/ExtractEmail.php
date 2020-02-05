02c99c7c4fde4204fb843c7416734d909699be1d
Chris Callender <chris.c@soniccode.com>
2020-02-04 16:20:21 -0800

    Added exceptions for Guzzle when results are not 200 Also added type hinting for returns, this makes php 7 a dependency now Created a validate url function just in case someone has provided bad data

--- src/ExtractEmail.php
+++ src/ExtractEmail.php
@@ -3,11 +3,16 @@
 namespace Chrsc\ExtractEmail;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
 
 class ExtractEmail
 {
 
     public const EMAIL_REGEX = '/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/';
+    public const URL_REGEX = '/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/';
+
+    /** @var string */
     private $url;
 
     public function __construct(string $url = null)
@@ -17,25 +22,58 @@ class ExtractEmail
         }
     }
 
+    /**
+     * @return $this
+     */
     public function setUrl(string $url): self
     {
-        $this->url = $url;
+        $this->url = $this->validateUrl($url) ? $url : null;
 
         return $this;
     }
 
-    public function getEmail()
+    /**
+     * @return mixed[]
+     */
+    public function getEmail(): array
     {
-        preg_match_all(SELF::EMAIL_REGEX, $this->getContents(), $matches);
+        if ($this->url === null) {
+            return [];
+        }
+        \preg_match_all(SELF::EMAIL_REGEX, $this->getContents(), $matches);
 
         return $matches[0];
     }
 
-    private function getContents()
+    /**
+     * @return string
+     */
+    private function getContents(): string
     {
         $client = new Client();
-        $response = $client->request('GET', $this->url);
+        try {
+            $response = $client->request('GET', $this->url);
+            if ($response->getStatusCode() === 200) {
+                return $response->getBody();
+            }
+        } catch (ClientException $clientException) {
+            \Log::error($clientException->getCode() . $clientException->getMessage());
+        } catch (RequestException $requestException) {
+            \Log::error($requestException->getCode() . $requestException->getMessage());
+        }
 
-        return $response->getBody();
+        return '';
+    }
+
+    /**
+     * @return bool
+     */
+    private function validateUrl(string $url): bool
+    {
+        if(\preg_match(self::URL_REGEX, $url) === 0) {
+            return (bool) \preg_match(self::URL_REGEX, 'http://' . $url);
+        }
+
+        return true;
     }
 }
